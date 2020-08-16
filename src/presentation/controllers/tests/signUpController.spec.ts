b9bf9c88dd09f19100fd0c34f561ff4f9e204e4a
tedu14 <thalison.eduardo14@gmail.com>
2020-08-15 22:48:10 -0300

    refactor: move email validation creation to a a factory helper method

--- src/presentation/controllers/tests/signUpController.spec.ts
+++ src/presentation/controllers/tests/signUpController.spec.ts
@@ -7,13 +7,26 @@ interface ISutTypes {
   emailValidatorStub: IEmailValidator
 }
 
-const makeSut = (): ISutTypes => {
+const makeEmailValidator = (): IEmailValidator => {
   class EmailValidatorStub implements IEmailValidator {
     isValid(email: string): boolean {
       return true
     }
   }
-  const emailValidatorStub = new EmailValidatorStub()
+  return new EmailValidatorStub()
+}
+
+const makeEmailValidatorWithError = (): IEmailValidator => {
+  class EmailValidatorStub implements IEmailValidator {
+    isValid(email: string): boolean {
+      throw new Error('Server internal error')
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+const makeSut = (): ISutTypes => {
+  const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
   return {
     sut,
@@ -132,12 +145,8 @@ describe('SignUp Controller', () => {
   })
 
   test('should return a status code 500 if email validator throws', () => {
-    class EmailValidatorStub implements IEmailValidator {
-      isValid(email: string): boolean {
-        throw new Error('Server internal error')
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub()
+    const emailValidatorStub = makeEmailValidatorWithError()
+
     const signUp = new SignUpController(emailValidatorStub)
 
     const httpRequest = {
